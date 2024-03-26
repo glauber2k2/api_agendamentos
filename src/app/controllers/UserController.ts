ff063d308b92e52573abc9c929182ba9dfee56e6
DevGlauber <devglaubermonteiro@gmail.com>
2024-03-26 17:42:56 -0300

    iniciado personalização da mensagem no email

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -29,11 +29,73 @@ class UserController {
 
       if (user) {
         const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-          expiresIn: '1h',
+          expiresIn: 15 * 60 * 1000, //15 minutos
         })
 
         const resetPasswordLink = `${process.env.APP_BASEURL}/nova-senha?token=${token}`
-        const emailHtml = `<b>Clique no link a seguir para redefinir sua senha:</b> <a href="${resetPasswordLink}">${resetPasswordLink}</a>`
+        const emailHtml = `
+        <!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8">
+              <style>
+                .container {
+                  margin: auto;
+                  max-width: 500px;
+                  height: 700px;
+                }
+                .header {
+                  background-color: #100f0f;
+                  color: white;
+                  padding: 8px;
+                  text-align: center;
+                }
+                .content {
+                  margin-top: 20px;
+                }
+                .content p{
+                  color: black;
+                  font-size: 14px;
+                  margin-bottom: 14px;
+                }
+                .btn, .btn:link, .btn:visited, .btn:hover, .btn:active {
+                  margin: auto;
+                  background-color: #7c3aed; 
+                  color: white !important; 
+                  padding: 10px 22px; 
+                  text-align: center;
+                  font-weight: 600;
+                  text-decoration: none; 
+                  display: inline-block; 
+                  font-size: 16px; 
+                  border: none; 
+                  cursor: pointer; 
+                  border-radius: 6px;
+                }
+
+                .btn:hover {
+                  background-color: #8243EF;
+                }
+            </style>
+            </head>
+            <body>
+            <div class="container">
+              <div class="header">
+                <h2>TimeAlign</h2>
+              </div>
+
+              <div class="content">
+                <p>Olá ${user.name},
+                Recebemos uma solicitação para redefinir sua senha do TimeAlign.
+                Insira o código de redefinição de senha a seguir:
+                </p>
+                <a href="${resetPasswordLink}" class="btn">Redefinir</a>
+              </div>
+            </div>
+
+            </body>
+        </html>
+        `
 
         await transporter.sendMail({
           from: `TimeAlign <${process.env.GMAIL_ACCOUNT}>`,
